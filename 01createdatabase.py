e5bf0f58fc1a7bb5d8f59c3727b3c6a9f139d25c
Fisafe <filipesfelisberto@gmail.com>
2016-11-17 14:32:52 -0200

    atividade 5
    atividade 5

---
+++ 01createdatabase.py
@@ -0,0 +1,6 @@
+
+# conectando e criando clientes.db
+# 01_create_db.py
+import sqlite3
+conn = sqlite3.connect('clientes.db')
+conn.close()
---
+++ 06readdata.py
@@ -0,0 +1,12 @@
+# 06_read_data.py
+import sqlite3
+
+conn = sqlite3.connect('clientes.db')
+cursor = conn.cursor()
+# lendo dados
+cursor.execute("""
+SELECT * FROM clientes;
+""")
+for linha in cursor.fetchall():
+    print(linha)
+conn.close()
